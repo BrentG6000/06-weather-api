79eeac3458d938bb5d529f8139e440b8f467a916
Brent Gustafson <50030696+BrentG6000@users.noreply.github.com>
2022-09-27 23:38:14 -0500

    Added code for weather api

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,7 @@
 const formEl = $('#location-form');
 const cityInputEl = $('#city-input');
 
-const ApiKey = "770feb5c85305a6a60e2a4039cf09a54";
+const ApiKey = "5050a3ebd6991b9594e93c4effbeb11c";
 let cityInput;
 //let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + ApiKey;
 let geoUrl = "http://api.openweathermap.org/geo/1.0/direct?q=";
@@ -12,7 +12,18 @@ let handleFormSubmit = e => {
     let cityInput = cityInputEl.val();
     console.log(cityInput);
     console.log(geoUrl + cityInput + "&limit=1&appid=" + ApiKey);
-    //fetch(geoUrl);
+    fetch(geoUrl).then(function (response) {
+        if (!response.ok) {
+            throw response.json();
+        }
+
+        return response.json();
+    }).then(function (locRes) {
+        // write query to page so user knows what they are viewing
+        
+
+        console.log(locRes);
+    })
 }
 
 formEl.on('submit', handleFormSubmit);
